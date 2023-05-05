f7cf8aacf6b649f79a937f37259fe35b8d4f41ec
ljh9 <dkssud2208@naver.com>
2023-05-05 22:15:31 +0900

    Add Dao

---
+++ src/UserDao.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDao {
+    private String filename;
+    public UserDao(String filename){
+        this.filename = filename;
+    }
+
+    public void saveUser(List<User> list){
+    try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
+        out.writeObject(list);
+    }catch (Exception ex){
+        ex.printStackTrace();
+    }
+    }
+
+    public List<User> getUser(){
+        File file = new File(filename);
+        if(!file.exists()){
+            return new ArrayList<>();
+        }
+
+        List<User> list = null;
+        try(ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))){
+            list = (List<User>)in.readObject();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return list;
+    }
+}
