f52853cfe807eaf206ff6f407790c94a35faa084
ljh9 <dkssud2208@naver.com>
2023-05-14 23:39:35 +0900

    Update alterUser

--- src/UserMain.java
+++ src/UserMain.java
@@ -18,6 +18,24 @@ public class UserMain {
                 System.out.println("등록");
             } else if(menuId == 2) {
                 userUI.printUserList(users);
+            } else if (menuId == 3) {
+                String email = userUI.inputEmail();
+                int findIndex = -1;
+                for (int i = 0; i < users.size(); i++){
+                    User u = users.get(i);
+                    if(u.getEmail().equals(email)){
+                        findIndex = i;
+                        break;
+                    }
+                }
+                if(findIndex != -1){
+                    User updateUser = userUI.inputUser(email);
+                    users.remove(findIndex);
+                    users.add(updateUser);
+                    System.out.println("수정완료");
+                }else {
+                    System.out.println("회원 없음");
+                }
             }
         }
 
