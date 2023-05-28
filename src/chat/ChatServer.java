40ba722461050bd3a327d727a28dc2b91ad80584
ljh9 <dkssud2208@naver.com>
2023-05-28 17:51:05 +0900

    update ChatServer

--- src/chat/ChatServer.java
+++ src/chat/ChatServer.java
@@ -1,28 +1,48 @@
 package chat;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ChatServer {
     public static void main(String[] args) throws Exception{
         ServerSocket serverSocket = new ServerSocket(9999);
-
+        List<PrintWriter> outList = Collections.synchronizedList(new ArrayList<>());
         while (true) {
             Socket socket = serverSocket.accept();
 
-            ChatThread chatThread = new ChatThread(socket);
+            ChatThread chatThread = new ChatThread(socket, outList);
             chatThread.start();
         }
     }
 }
 class ChatThread extends Thread{
     private Socket socket;
+    private List<PrintWriter> outList;
+    private PrintWriter out;
+    private BufferedReader in;
 
-    public ChatThread(Socket socket){
+    public ChatThread(Socket socket, List<PrintWriter> outList){
         this.socket = socket;
+        this.outList = outList;
+
+        try {
+            out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (Exception ex) {
+            ex.printStackTrace();;
+        }
+
     }
 
     public void run(){
+
 
     }
 }
