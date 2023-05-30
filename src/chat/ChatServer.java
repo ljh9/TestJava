39f58b05c57570d5e4cf16802e7359c90de61d6e
ljh9 <dkssud2208@naver.com>
2023-05-30 22:44:28 +0900

    chatServer test

--- src/chat/ChatServer.java
+++ src/chat/ChatServer.java
@@ -1,9 +1,6 @@
 package chat;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -16,6 +13,7 @@ public class ChatServer {
         List<PrintWriter> outList = Collections.synchronizedList(new ArrayList<>());
         while (true) {
             Socket socket = serverSocket.accept();
+            System.out.println("접속 : " + socket);
 
             ChatThread chatThread = new ChatThread(socket, outList);
             chatThread.start();
@@ -50,6 +48,7 @@ class ChatThread extends Thread{
                 for (int i = 0; i< outList.size(); i++) {
                     PrintWriter o = outList.get(i);
                     o.println(line);
+                    o.flush();
                 }
             }
         } catch (Exception ex) {
@@ -59,6 +58,17 @@ class ChatThread extends Thread{
                 outList.remove(out);
             }catch (Exception ex){
                 ex.printStackTrace();
+            }
+            for (int i = 0; i< outList.size(); i++) {
+                PrintWriter o = outList.get(i);
+                o.println("끊어짐");
+                o.flush();
+            }
+
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
 
