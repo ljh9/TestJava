ef504b2b0fbeee55bd35c1ff85cc051398999fba
ljh9 <dkssud2208@naver.com>
2023-05-31 21:32:33 +0900

    add chatClient

--- src/chat/ChatClient.java
+++ src/chat/ChatClient.java
@@ -1,4 +1,23 @@
 package chat;
 
+import java.io.*;
+import java.net.Socket;
+
 public class ChatClient {
+    public static void main(String[] args) throws Exception{
+        String name = args[0];
+
+        Socket socket = new Socket("127.0.0.1", 999);
+
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = null;
+
+        while ((line = keyboard.readLine()) != null){
+            out.println(name + " : " + line);
+            out.flush();
+        }
+    }
 }
