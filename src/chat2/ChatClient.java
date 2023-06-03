88acc0ea119c5102dd9066140c42b76b87e3f4a8
ljh9 <dkssud2208@naver.com>
2023-06-04 00:25:56 +0900

    add chatClient

---
+++ src/chat2/ChatClient.java
@@ -0,0 +1,22 @@
+package chat2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ChatClient extends Thread{
+    private String name;
+    private BufferedReader br;
+    private PrintWriter pw;
+    private Socket socket;
+
+    public ChatClient(Socket socket) throws Exception{
+        this.socket = socket;
+        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        this.br = br;
+        this.pw = pw;
+    }
+}
