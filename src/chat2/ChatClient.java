a46535d25c065b8964a706c5df69ed248b6c03ea
ljh9 <dkssud2208@naver.com>
2023-06-05 19:11:55 +0900

    update chatClient

--- src/chat2/ChatClient.java
+++ src/chat2/ChatClient.java
@@ -5,18 +5,22 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.List;
 
 public class ChatClient extends Thread{
     private String name;
     private BufferedReader br;
     private PrintWriter pw;
     private Socket socket;
+    List<ChatClient> list;
 
-    public ChatClient(Socket socket) throws Exception{
+    public ChatClient(Socket socket, List<ChatClient> list) throws Exception{
         this.socket = socket;
         BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
         this.br = br;
         this.pw = pw;
+        this.name = br.readLine();
+        this.list.add(this);
     }
 }
