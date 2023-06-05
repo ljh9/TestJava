55dcf969d6d88054f1bf8c638cf1c128b38a18ff
ljh9 <dkssud2208@naver.com>
2023-06-06 00:24:38 +0900

    update chatServer

--- src/chat2/ChatServer.java
+++ src/chat2/ChatServer.java
@@ -2,10 +2,18 @@ package chat2;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ChatServer {
     public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = new ServerSocket(8888);
-        Socket socket = serverSocket.accept();
+        List<ChatClient> list = Collections.synchronizedList(new ArrayList<>());
+        while (true){
+            Socket socket = serverSocket.accept();
+            ChatClient chatClient = new ChatClient(socket, list);
+            chatClient.start();
+        }
     }
 }
