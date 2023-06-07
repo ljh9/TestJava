89a81197cb2442dcba322a18d42977d5ebde821a
ljh9 <dkssud2208@naver.com>
2023-06-08 00:27:13 +0900

    add chatClient

---
+++ src/chat2/ChatClient.java
@@ -0,0 +1,22 @@
+package chat2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ChatClient {
+    public static void main(String[] args) throws Exception {
+        if(args.length != 1 ){
+            System.out.println("닉네임");
+            return;
+        }
+        String name = args[0];
+        Socket socket = new Socket("127.0.0.1", 8888);
+        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+
+    }
+}
